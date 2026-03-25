555e8a2cc367cb588f41eb40d63008cefd8c66ac
Lucas Amim <lucas.amim@hotmail.com>
2026-03-24 21:22:25 -0300

    fix: adiciona base path para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://lucasamim.github.io',
+  base: '/juh-vieira-porao-das-quinquilharias',
   vite: {
     plugins: [tailwindcss()]
   }
